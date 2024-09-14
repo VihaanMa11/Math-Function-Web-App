c89128ab90bf42c2817182fc35d604e064b202b7
VihaanMa11 <vihaanmalani94@gmail.com>
2024-09-14 21:51:49 +0530

    multiply 1

--- app.py
+++ app.py
@@ -31,12 +31,22 @@ def modulo():
     result = number1 % number2
     return jsonify(result=result)
 
+<<<<<<< HEAD
+@app.route('/multiply', methods=['POST'])
+def multiply():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    result = num1 * num2
+    return jsonify(result=result)    
+
+=======
 def sqrt():
     num = int(input("Enter a Number: "))
     if num<0:
         return "not possible"
     return math.sqrt(num)
 
+>>>>>>> 9a744ae9c9d3956fc4fea9a84438d67d128be768
 # You can add more routes for each math function
 # for example, multiplication, square roots, etc.
 
